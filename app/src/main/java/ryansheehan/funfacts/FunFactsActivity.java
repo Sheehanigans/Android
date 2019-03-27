58110d67138102388e204105dcc1780e9ea58cea
Sheehanigans <RyanSheehan22@gmail.com>
2019-03-26 19:40:03 -0500

    added ui and facts

--- app/src/main/java/ryansheehan/funfacts/FunFactsActivity.java
+++ app/src/main/java/ryansheehan/funfacts/FunFactsActivity.java
@@ -1,13 +1,55 @@
 package ryansheehan.funfacts;
 
+import android.app.SearchManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
+    //declare the view variables
+    private TextView factTextView;
+    private Button showFactButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        //Assign the view from the layout file to the corresponding variables
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach Earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built.",
+                        "Treehouse is not actually in a tree."};
+
+                //the button was clicked to update the fact text view with a new fact
+                //randomly select a fact
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.nextInt(facts.length);
+                String fact = facts[randomNumber];
+
+                //update the screen with our new fact
+                factTextView.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 }
